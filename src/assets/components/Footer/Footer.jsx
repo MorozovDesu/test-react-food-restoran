1782b16cb29148ebd0aad2c1900dedb88b7e9e3e
MDesu <2dpunk3@mail.ru>
2025-10-25 21:07:12 +0800

    Осталось сделть адаптив для футера

--- src/assets/components/Footer/Footer.jsx
+++ src/assets/components/Footer/Footer.jsx
@@ -1,8 +1,13 @@
 
-
+import FooterChapter from "../FooterChapter/FooterChapter.jsx";
 import "./Footer.css";
 
-
 export default function Footer() {
-  return <footer className="footer">FOOTER</footer>;
+  return <footer className="footer">
+    <FooterChapter></FooterChapter>
+    <FooterChapter></FooterChapter>
+    <FooterChapter></FooterChapter>
+    <FooterChapter></FooterChapter>
+  </footer>
+  ;
 }
---
+++ src/assets/components/FooterChapter/FooterChapter.jsx
@@ -0,0 +1,12 @@
+
+import "./FooterChapter.css";
+
+export default function FooterChapter() {
+  return <footer className="footerChapter">
+    <h3>Раздел </h3>
+    <h4 className="footerChapterText">Подраздел 1</h4>
+    <h4 className="footerChapterText">Подраздел 2</h4>
+    <h4 className="footerChapterText">Подраздел 3</h4>
+    <h4 className="footerChapterText">Подраздел 4</h4>
+  </footer>;
+}
